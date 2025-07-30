02cc3c59f63290a71e4fa18bf59797ecd8822b55
jyp <firepooh77@google.com>
2025-07-31 00:54:18 +0900

    Remove VSCode settings file and implement SHT3x temperature and humidity sensor integration with periodic notifications

--- main/app_main.cpp
+++ main/app_main.cpp
@@ -28,6 +28,15 @@
 #include <i2c_bus.h>
 #include <sht3x.h>
 
+#define I2C_MASTER_SCL_IO  GPIO_NUM_2
+#define I2C_MASTER_SDA_IO  GPIO_NUM_3
+#define I2C_MASTER_NUM     I2C_NUM_0
+#define I2C_MASTER_FREQ_HZ 100000
+
+static i2c_bus_handle_t i2c_bus = NULL;
+static sht3x_handle_t sht3x = NULL;
+
+
 static const char *TAG = "app_main";
 
 using namespace esp_matter;
@@ -35,7 +44,69 @@ using namespace esp_matter::attribute;
 using namespace esp_matter::endpoint;
 using namespace chip::app::Clusters;
 
+using shtc3_sensor_cb_t = void (*)(uint16_t endpoint_id, float value, void *user_data);
+
+typedef struct {
+    struct {
+        // This callback functon will be called periodically to report the temperature.
+        shtc3_sensor_cb_t cb = NULL;
+        // endpoint_id associated with temperature sensor
+        uint16_t endpoint_id;
+    } temperature;
+
+    struct {
+        // This callback functon will be called periodically to report the humidity.
+        shtc3_sensor_cb_t cb = NULL;
+        // endpoint_id associated with humidity sensor
+        uint16_t endpoint_id;
+    } humidity;
+
+    // user data
+    void *user_data = NULL;
+
+    // polling interval in milliseconds, defaults to 5000 ms
+    uint32_t interval_ms = 10000;
+} shtc3_sensor_config_t;
+
 constexpr auto k_timeout_seconds = 300;
+
+// Application cluster specification, 7.18.2.11. Temperature
+// represents a temperature on the Celsius scale with a resolution of 0.01°C.
+// temp = (temperature in °C) x 100
+static void temp_sensor_notification(uint16_t endpoint_id, float temp, void *user_data)
+{
+    // schedule the attribute update so that we can report it from matter thread
+    chip::DeviceLayer::SystemLayer().ScheduleLambda([endpoint_id, temp]() {
+        attribute_t * attribute = attribute::get(endpoint_id,
+                                                 TemperatureMeasurement::Id,
+                                                 TemperatureMeasurement::Attributes::MeasuredValue::Id);
+
+        esp_matter_attr_val_t val = esp_matter_invalid(NULL);
+        attribute::get_val(attribute, &val);
+        val.val.i16 = static_cast<int16_t>(temp * 100);
+
+        attribute::update(endpoint_id, TemperatureMeasurement::Id, TemperatureMeasurement::Attributes::MeasuredValue::Id, &val);
+    });
+}
+
+// Application cluster specification, 2.6.4.1. MeasuredValue Attribute
+// represents the humidity in percent.
+// humidity = (humidity in %) x 100
+static void humidity_sensor_notification(uint16_t endpoint_id, float humidity, void *user_data)
+{
+    // schedule the attribute update so that we can report it from matter thread
+    chip::DeviceLayer::SystemLayer().ScheduleLambda([endpoint_id, humidity]() {
+        attribute_t * attribute = attribute::get(endpoint_id,
+                                                 RelativeHumidityMeasurement::Id,
+                                                 RelativeHumidityMeasurement::Attributes::MeasuredValue::Id);
+
+        esp_matter_attr_val_t val = esp_matter_invalid(NULL);
+        attribute::get_val(attribute, &val);
+        val.val.u16 = static_cast<uint16_t>(humidity * 100);
+
+        attribute::update(endpoint_id, RelativeHumidityMeasurement::Id, RelativeHumidityMeasurement::Attributes::MeasuredValue::Id, &val);
+    });
+}
 
 static void app_event_cb(const ChipDeviceEvent *event, intptr_t arg)
 {
@@ -126,14 +197,7 @@ static esp_err_t app_attribute_update_cb(attribute::callback_type_t type, uint16
     return err;
 }
 
-#define I2C_MASTER_SCL_IO  GPIO_NUM_3
-#define I2C_MASTER_SDA_IO  GPIO_NUM_4
-#define I2C_MASTER_NUM     I2C_NUM_0
-#define I2C_MASTER_FREQ_HZ 100000
-
-static i2c_bus_handle_t i2c_bus = NULL;
-static sht3x_handle_t sht3x = NULL;
-
+static esp_err_t sensor_get( float *temperature, float *humidity);
 static void sensor_init(void)
 {
     // Initialize I2C for sensor communication
@@ -161,22 +225,102 @@ static void sensor_init(void)
     }
 
     sht3x_set_measure_mode(sht3x, SHT3x_PER_2_MEDIUM);     /*!< here read data in periodic mode*/
+
+    for( int i = 0 ; i < 10; i++ ) {
+        float temperature = 0.0f;
+        float humidity = 0.0f;
+        sensor_get(&temperature, &humidity);
+        vTaskDelay(pdMS_TO_TICKS(1000)); // Delay for 1 second before next reading
+    }
 }
 
-void sensor_get( float *temperature, float *humidity)
+static esp_err_t sensor_get( float *temperature, float *humidity)
 {
     if (sht3x == NULL) {
         ESP_LOGE(TAG, "SHT3x sensor handle is NULL");
-        return;
+        return ESP_ERR_INVALID_STATE;
     }
 
     esp_err_t err = sht3x_get_humiture(sht3x, temperature, humidity);
     if (err != ESP_OK) {
         ESP_LOGE(TAG, "Failed to get sensor data, err: %d", err);
-        return;
+        return err;
     }
 
     ESP_LOGI(TAG, "Temperature: %.2f C, Humidity: %.2f %%", *temperature, *humidity);
+    return ESP_OK;
+}
+
+typedef struct {
+    shtc3_sensor_config_t *config;
+    esp_timer_handle_t timer;
+    bool is_initialized = false;
+} shtc3_sensor_ctx_t;
+
+static shtc3_sensor_ctx_t s_ctx;
+
+static void timer_cb_internal(void *arg)
+{
+    auto *ctx = (shtc3_sensor_ctx_t *) arg;
+    if (!(ctx && ctx->config)) {
+        return;
+    }
+
+    float temp, humidity;
+    esp_err_t err = sensor_get(&temp, &humidity);
+    if (err != ESP_OK) {
+        return;
+    }
+    if (ctx->config->temperature.cb) {
+        ctx->config->temperature.cb(ctx->config->temperature.endpoint_id, temp, ctx->config->user_data);
+    }
+    if (ctx->config->humidity.cb) {
+        ctx->config->humidity.cb(ctx->config->humidity.endpoint_id, humidity, ctx->config->user_data);
+    }
+}
+
+static esp_err_t sensor_timer_init( shtc3_sensor_config_t *config )
+{
+    esp_err_t err = ESP_OK;
+
+    if (config == NULL) {
+        return ESP_ERR_INVALID_ARG;
+    }
+    // we need at least one callback so that we can start notifying application layer
+    if (config->temperature.cb == NULL || config->humidity.cb == NULL) {
+        return ESP_ERR_INVALID_ARG;
+    }
+    if (s_ctx.is_initialized) {
+        return ESP_ERR_INVALID_STATE;
+    }
+
+    // keep the pointer to config
+    s_ctx.config = config;    
+
+    esp_timer_create_args_t args = {
+        .callback = timer_cb_internal,
+        .arg = &s_ctx,
+    };
+
+    err = esp_timer_create(&args, &s_ctx.timer);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "esp_timer_create failed, err:%d", err);
+        return err;
+    }
+
+    #if 1
+    err = esp_timer_start_periodic(s_ctx.timer, config->interval_ms * 1000);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "esp_timer_start_periodic failed: %d", err);
+        return err;
+    }
+    #endif
+
+
+    s_ctx.is_initialized = true;
+    ESP_LOGI(TAG, "shtc3 initialized successfully");
+    
+    return ESP_OK;
 }
 
 extern "C" void app_main()
@@ -211,6 +355,30 @@ extern "C" void app_main()
     endpoint_t *app_endpoint = endpoint::on_off_light::create(node, &endpoint_config, ENDPOINT_FLAG_NONE, NULL);
     ABORT_APP_ON_FAILURE(app_endpoint != nullptr, ESP_LOGE(TAG, "Failed to create on off light endpoint"));
 
+    // add temperature sensor device
+    temperature_sensor::config_t temp_sensor_config;
+    endpoint_t * temp_sensor_ep = temperature_sensor::create(node, &temp_sensor_config, ENDPOINT_FLAG_NONE, NULL);
+    ABORT_APP_ON_FAILURE(temp_sensor_ep != nullptr, ESP_LOGE(TAG, "Failed to create temperature_sensor endpoint"));
+
+    // add the humidity sensor device
+    humidity_sensor::config_t humidity_sensor_config;
+    endpoint_t * humidity_sensor_ep = humidity_sensor::create(node, &humidity_sensor_config, ENDPOINT_FLAG_NONE, NULL);
+    ABORT_APP_ON_FAILURE(humidity_sensor_ep != nullptr, ESP_LOGE(TAG, "Failed to create humidity_sensor endpoint"));
+
+    static shtc3_sensor_config_t shtc3_config = {
+        .temperature = {
+            .cb = temp_sensor_notification,
+            .endpoint_id = endpoint::get_id(temp_sensor_ep),
+        },
+        .humidity = {
+            .cb = humidity_sensor_notification,
+            .endpoint_id = endpoint::get_id(humidity_sensor_ep),
+        },
+    };    
+
+    err = sensor_timer_init( &shtc3_config );
+    ABORT_APP_ON_FAILURE(err == ESP_OK, ESP_LOGE(TAG, "Failed to initialize temperature sensor driver"));
+
 #if CHIP_DEVICE_CONFIG_ENABLE_THREAD
     /* Set OpenThread platform config */
     esp_openthread_platform_config_t config = {
@@ -220,6 +388,9 @@ extern "C" void app_main()
     };
     set_openthread_platform_config(&config);
 #endif
+
+    
+    
 
     /* Matter start */
     err = esp_matter::start(app_event_cb);
